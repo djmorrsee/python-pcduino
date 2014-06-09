3190b188efb28231918c0d9413888f80f3c5165a
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-09 12:27:50 -0600

    Data Collection and Server Communication

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ####Forked from https://github.com/pcduino/python-pcduino
 
-The deployment branch for the arduino. 
+The deployment branch for the arduino.
 
 This is setup to be cloned somewhere on the pcDuino and just run. The python-pcDuino package is included
+
+
+## Take_Reading.py
+
+A standalone python program meant to be run on a pcDuino.
--- take_reading.py
+++ take_reading.py
@@ -1,7 +1,5 @@
 from bin.pcduino.adc import analog_read
-
 from bin.network.send_data import *
-
 from bin.util.calibration import *
 import random
 
@@ -14,6 +12,8 @@ LIGHT_PIN = 2
 
 THIS_MODULE_NUMBER = 1 # Set yours to 2 if you notice!!!
 
+SERVER_URL = 'http://127.0.0.1:5000/post_reading'
+
 #Use this for the duino
 def GetReading(pin):
 	return SmoothReading(analog_read, 10, pin)
@@ -25,12 +25,13 @@ def GetDummyReading(pin):
 def Main():
 	# Read Temp
 	temp = SmoothReading(GetDummyReading, 10, 100, TEMP_PIN)
+	#temp = GetReading(TEMP_PIN)
 
 	# Read Light
 	light = SmoothReading(GetDummyReading, 10, 100, LIGHT_PIN)
+	#light = GetReading(LIGHT_PIN)
 
 	# Send Data
-	url = 'http://127.0.0.1:5000/dj_echo/'
-	PostData(url, temp, light, THIS_MODULE_NUMBER)
+	PostData(SERVER_URL, temp, light, THIS_MODULE_NUMBER)
 
 Main()
