34e1f8a24013ae0d263fd20276a367d621f29941
Andrey Shprengel <andrey.shprengel@colorado.edu>
2014-06-10 12:42:26 -0600

    testsendcode changed

--- testsendcode.py
+++ testsendcode.py
@@ -1,4 +1,5 @@
 import requests, json
 payload ={'temp' : 75}
 headers = {'content-type': 'application/json'}
-r = requests.post("http://127.0.0.1:5000/echo/", data=json.dumps(payload),headers = headers)
+r = requests.post("http://remote-light.herokuapp.com/echo/", data=json.dumps(payload),headers = headers)
+print r.text
