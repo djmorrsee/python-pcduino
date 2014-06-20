bb882481c83a13057c2cac4f2b866fffdcd4330a
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-20 12:16:22 -0600

    Removed sendcode.py

--- sendcode.py
+++
@@ -1,7 +0,0 @@
-import requests, json
-import hardware_functions
-current_temp = hardware_functions.readTemp()
-payload ={'temp' : current_temp}
-headers = {'content-type': 'application/json'}
-r = requests.post("http://remote-light.herokuapp.com/echo/", data=json.dumps(payload),headers = headers)
-print r.text
