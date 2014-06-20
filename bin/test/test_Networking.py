b88b2eba92788b2150c11a10e9aef427a7096012
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-20 12:12:36 -0600

    Test Fixes, Conversion Removal

--- bin/test/test_Networking.py
+++ bin/test/test_Networking.py
@@ -10,31 +10,29 @@ from bin.network.send_data import *
 
 class NetworkTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        pass
+  @classmethod
+  def setUpClass(cls):
+    pass
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
+  @classmethod
+  def tearDownClass(cls):
+    pass
 
-    def setUp(self):
-        pass
+  def setUp(self):
+    pass
 
-    def tearDown(self):
-        pass
-        
-	def test_normal_send(self):
-		url = "http://remote-light.herokuapp.com/post_reading"
-		light = 95
-		temp = 84
-		m_id = 2
-		val = PostData(url, temp, light, m_id)
-		self.assertEqual(val, 'Success')
+  def tearDown(self):
+    pass
+
+  def test_normal_send(self):
+    url = "http://remote-light.herokuapp.com/post_reading"
+    light = 95
+    temp = 84
+    m_id = 2
+    val = PostData(url, temp, light, m_id)
+    self.assertEqual(val, 'Success')
 
 
 # Main: Run Test Cases
 if __name__ == '__main__':
     unittest.main()
-
-
--- take_reading.py
+++ take_reading.py
@@ -1,7 +1,6 @@
 from bin.pcduino.adc import analog_read
 from bin.network.server_comm import *
 from bin.util.constants import *
-from bin.util.hardware_conversions import *
 from bin.util.calibration import *
 import random
 
@@ -19,18 +18,15 @@ def GetDummyReading(pin):
 def Main():
 	# Read Temp
 	temp_pin_reading = SmoothReading(GetDummyReading, 10, 100, TEMP_PIN)
-	temp = GetTemp(temp_pin_reading)
+	# temp = GetTemp(temp_pin_reading)
 
 
 	# Read Light
 	light_pin_reading = SmoothReading(GetDummyReading, 10, 100, LIGHT_PIN)
 	#light = GetReading(LIGHT_PIN)
-	light = GetLight(light_pin_reading)
 
 	# Send Data
-	json_data = FormJSONData(MODULE_ID, MODULE_AUTH_ID, temp, light)
+	json_data = FormJSONData(MODULE_ID, MODULE_AUTH_ID, temp_pin_reading, light_pin_reading)
 	PostJSONToServer(json_data, SERVER_URL)
-
-	#PostData(SERVER_URL, temp, light, MODULE_ID)
 
 Main()
