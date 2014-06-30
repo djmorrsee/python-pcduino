8ed9c09b9d8a22da8057e593f63a0876c4e1a923
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-30 10:58:32 -0600

    abs auth path

--- bin/util/authorization.py
+++ bin/util/authorization.py
@@ -1,14 +1,18 @@
 ## @package Module Authorization Code
 
+import os
+this_dir = os.path.dirname(__file__)
+auth_file_path = os.path.join(this_dir, 'm_auth_id.txt')
+
 ## Saves authorizatioin ID during registration
 #@param m_auth_id given at reistaration
 def SaveModuleAuthorizationID(m_auth_id):
-	file = open('bin/util/m_auth_id.txt', 'w+')
+	file = open(auth_file_path, 'w+')
 	file.write(m_auth_id)
 	file.close()
 ##Returns the authorization id if there is one
 def GetModuleAuthorizationID():
-	file = open('bin/util/m_auth_id.txt', 'r')
+	file = open(auth_file_path, 'r')
 	if file:
 		return file.readlines()[0]
 	else:
