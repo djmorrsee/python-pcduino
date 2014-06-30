3adb3c541faccdbed3f92ec95a1dad0a14819d09
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-30 11:01:28 -0600

    clean auth.py

--- bin/util/authorization.py
+++ bin/util/authorization.py
@@ -17,27 +17,3 @@ def GetModuleAuthorizationID():
 		return file.readlines()[0]
 	else:
 		return None
-
-
-
-# auth_ids = []
-#
-# def BuildAuthIDs():
-# 	auth_file = open('auth_file.txt', 'r')
-#
-# 	auth_ids = []
-# 	for _id in auth_file.readlines():
-# 		auth_ids.append()
-#
-# 	auth_file.close()
-#
-# def VerifyAuthData(data):
-# 	return data.keys() == ["auth_id"]
-#
-# def VerifyModuleData(data):
-# 	return data.keys() == ["module_auth_id", "module_id", "reading"]
-#
-# def AuthorizeAuthData(data):
-# 	return data["auth_id"] in auth_ids
-#
-# BuildAuthIDs()
