3cc7c63ddb6e563b436d173c69edef203e8115a8
Daniel Morrissey <djmorrsee@gmail.com>
2014-06-30 22:24:07 -0600

    Seperating duino python programs

---
+++ unregister_module.py
@@ -0,0 +1,15 @@
+from bin.network.server_comm import DeleteJSONToServer
+from bin.util.authorization import FormRegistrationJSON
+from bin.util.constants import MODULE_ID, AUTH_ID
+
+# SERVER_URL = 'http://remote-light.herokuapp.com/module/%i/' % MODULE_ID
+SERVER_URL = 'http://192.168.1.108:5000/module/%i/' % MODULE_ID
+
+##Function for removoving  the pCduino from the server
+def UnRegister():
+
+		data = FormRegistrationJSON(AUTH_ID)
+		r = DeleteJSONToServer(data, SERVER_URL)
+		print(r.status_code)
+
+UnRegister()
