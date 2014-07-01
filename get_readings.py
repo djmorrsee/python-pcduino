42fd5d1f2a0932b29256d7f9deb3bd90d4dc1a83
Daniel Morrissey <djmorrsee@gmail.com>
2014-07-01 11:41:10 -0600

    Get Readings Program

--- get_readings.py
+++ get_readings.py
@@ -2,7 +2,7 @@ from bin.network.server_comm import *
 from bin.util.constants import MODULE_ID
 from pprint import pprint
 
-SERVER_URL = 'http://127.0.0.1:5000/module/%i/' % MODULE_ID
+SERVER_URL = 'http://192.168.1.108:5000/module/%i/' % MODULE_ID
 
 def GetReadings():
 	data = GetDataFromServer(SERVER_URL)
